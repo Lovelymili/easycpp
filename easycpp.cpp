2ba958ba5ff21c3040020f486620d7d49c1a7f2e
Lovelymili <144820752+Lovelymili@users.noreply.github.com>
2024-09-20 09:52:28 +0800

    Add files via upload

--- easycpp.cpp
+++ easycpp.cpp
@@ -11,3 +11,39 @@ int quxwei(int m,int n)  //m是原数字,n是第要取的第几位
 	m = m % 10;
 	return m;
 }
+
+double sishewuru(double x , int n) //x是需要四舍五入的数字,n是四舍五入到第几位
+{
+	try {
+		cin >> x;
+		if (cin.fail())
+			throw invalid_argument("参数x不是数字");
+		try {
+			cin >> n;
+			if (cin.fail())
+				throw invalid_argument("参数n不是整数");
+			x = x * pow(10, n);
+			if (x - floor(x) >= 0, 5)
+			{
+				x = x - (x - floor(x));
+				++x;
+				x = x / 100;
+			}
+			else
+			{
+				x = x - (x - floor(x));
+				x = x / 100;
+			}
+			return x;
+		}
+		catch (invalid_argument)
+		{
+			cout << "sishewuru函数参数n输入错误!";
+		}
+	}
+	catch (invalid_argument)
+	{
+		cout << "sishewuru函数参数m输入错误!";
+	}
+
+}
